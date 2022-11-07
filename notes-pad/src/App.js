21794aecae0791e64977d7e4c22518247b45f6b7
Yonah Diaz <108345049+YonahDiaz@users.noreply.github.com>
2022-11-07 16:29:55 +0200

    Some fixes

--- notes-pad/src/App.js
+++ notes-pad/src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+function NotesAndButton() {
+  const [note, setNote] = useState([]);
+
+  const addNoteOnClick = () => {
+    setNote((noteArray) => [...noteArray, `${new Date().toLocaleString()}`]);
+  };
+
+  const deleteNoteButton = () => {
+    console.log("delete");
+  };
+
+  return [
+    <div>
+      {note.map((note, index) => (
+        <div key={index} className="Note">
+          <div className="Note-title">{"Note title:"}</div>
+          <div className="IndividualNotes">
+            {"Example note"}
+            <p></p>
+            {note}
+          </div>
+          <p></p>
+
+          <input
+            type="button"
+            onClick={deleteNoteButton}
+            className="Remove-note-button"
+            value="Remove Note"
+          />
+        </div>
+      ))}
+    </div>,
+
+    <input
+      type="button"
+      className="Add-note-button"
+      onClick={addNoteOnClick}
+      value="Add note"
+    />,
+  ];
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Notes-grid">
+          <NotesAndButton />
+        </div>
       </header>
     </div>
   );
